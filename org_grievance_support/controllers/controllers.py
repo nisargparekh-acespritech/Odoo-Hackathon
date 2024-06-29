094a6649da066681f445f74441e5bef1bd133f30
Nisarg Parekh <nisarg.parekh@iviewlabs.com>
2024-06-29 12:43:16 +0530

    [ADD]: minor changes

--- org_grievance_support/controllers/controllers.py
+++ org_grievance_support/controllers/controllers.py
@@ -4,7 +4,7 @@ from odoo.http import request
 
 class OrgGrievanceSupport(http.Controller):
 
-    @http.route('/my_controller', type='http', auth='public', website=True)
+    @http.route('/grievance/registration', type='http', auth='public', website=True)
     def document_upload(self, **kw):
         return request.render("org_grievance_support.my_controller_form")
 
