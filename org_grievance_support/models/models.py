f4226af31c8c28f50b56761f99ee24e63a001329
Harshil-SD-SA <harshil@strategicdimensions.co.za>
2024-06-29 11:40:21 +0530

    [ADD] controller and registration form

--- org_grievance_support/models/models.py
+++ org_grievance_support/models/models.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class org_grievance_support(models.Model):
-#     _name = 'org_grievance_support.org_grievance_support'
-#     _description = 'org_grievance_support.org_grievance_support'
+class org_grievance_support(models.Model):
+    _name = 'org_grievance_support'
+    _description = 'org_grievance_support'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    description = fields.Text()
 
+    def action_channel_enroll(self):
+    	return {
+            'type': 'ir.actions.act_url',
+            'url': '/my_controller',
+            'target': 'self',
+        }
