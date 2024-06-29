b426b2f21c4624bb08ef97b91343442c61f2abda
Nisarg Parekh <nisarg.parekh@iviewlabs.com>
2024-06-29 12:01:35 +0530

    [UPD]:Code updated

--- org_grievance_support/__manifest__.py
+++ org_grievance_support/__manifest__.py
@@ -1,22 +1,32 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Org Grieviance Support",
-    'summary': "Org Grieviance Support",
-    'description': """Org Grieviance Support""",
-    'author': "Harshil",
-    'website': "",
-    'category': '',
-    'version': '17.0.0.0',
-    'depends': ['base','website'],
+    'name': "org_grievance_support",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr','website'],
+
+    # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
         'views/templates.xml',
+        'data/grievance_type.xml',
+        'views/employee_grievance_views.xml',
+        'views/grievance_type_views.xml',
     ],
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
+    # only loaded in demonstration mode
 }
-
-
