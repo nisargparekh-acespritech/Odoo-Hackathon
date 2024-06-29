9f7dd7e9c8708d44fa7ef479c051ba3f6469774e
Nisarg Parekh <nisarg.parekh@iviewlabs.com>
2024-06-29 17:42:02 +0530

    [upd]:chagnes

--- org_grievance_support/controllers/controllers.py
+++ org_grievance_support/controllers/controllers.py
@@ -25,3 +25,64 @@ class OrgGrievanceSupport(http.Controller):
         })
         value = {'grievance_id':grievance_id}
         return request.render("org_grievance_support.RegisterMessage",value)
+
+    @http.route('/grievance/count', auth='public', type='json')
+    def get_grievance_count(self):
+        grievance_ids = request.env['employee.grievance'].search([])
+        grievance_counts = {
+            'pending': 0,
+            'on_going': 0,
+            'resolved': 0
+        }
+
+        for grievance in grievance_ids:
+            if grievance.status in grievance_counts:
+                grievance_counts[grievance.status] += 1
+
+        grievance_state = ['Pending', 'On Going', 'Resolved']
+        total_grievance = [
+            grievance_counts['pending'],
+            grievance_counts['on_going'],
+            grievance_counts['resolved']
+        ]
+        colors = ['#F0FFFF', '#ADD8E6', '#87CEFA']
+
+        return {
+            'grievance': grievance_state,
+            'total': total_grievance,
+            'color': colors
+        }
+
+    @http.route('/grievance/bar', auth='public', type='json')
+    def get_top_timesheet_employees(self):
+        grievance_ids = request.env['employee.grievance'].search([])
+        grievance_counts = {
+            'pending': 0,
+            'on_going': 0,
+            'resolved': 0
+        }
+
+        for grievance in grievance_ids:
+            if grievance.status in grievance_counts:
+                grievance_counts[grievance.status] += 1
+
+        total_count = [
+            grievance_counts['pending'],
+            grievance_counts['on_going'],
+            grievance_counts['resolved']
+        ]
+        state = ['Pending', 'On Going', 'Resolved']
+
+        return [total_count, state]
+
+    @http.route('/grievance/datacount', auth='public', type='json')
+    def get_job_order_details(self):
+        grievance_ids = request.env['employee.grievance'].search([])
+        pending_count = len(grievance_ids.filtered(lambda x:x.status == 'pending'))
+        ongoing_count = len(grievance_ids.filtered(lambda x:x.status == 'on_going'))
+        resolved_count = len(grievance_ids.filtered(lambda x:x.status == 'resolved'))
+        return {
+            'pending_count': pending_count,
+            'ongoing_count': ongoing_count,
+            'resolved_count': resolved_count,
+        }
